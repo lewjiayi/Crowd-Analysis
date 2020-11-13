8b83b88f7f65c6febecb3e407be1063bd94b4a7a
lewjiayi <jiaareaper@hotmail.com>
2020-11-13 17:54:06 +0800

    limit the heatmap blob max size

--- movement_data_present.py
+++ movement_data_present.py
@@ -43,6 +43,14 @@ with open('processed_data/video_data.json', 'r') as file:
 stationary_threshold_seconds = 2
 stationary_threshold_frame =  round(vid_fps * stationary_threshold_seconds / data_record_frame)
 stationary_distance = frame_size * 0.05
+max_stationary_time = 120
+blob_layer = 50
+max_blob_size = frame_size * 0.1
+layer_size = max_blob_size / blob_layer
+color_start = 210
+color_end = 0
+color_steps = int((color_start - color_end) / blob_layer)
+scale = 1.5
 
 stationary_points = []
 for movement in tracks:
@@ -57,31 +65,40 @@ for movement in tracks:
 				stationary_points.append([stationary, stationary_time])
 			stationary = i
 			stationary_time = 0
-        
-def draw_blob(frame, coordinates, weight, frame_size):
-    min_size = frame_size * 0.0001
-    base_color = 8
-    for x in reversed(range(32)):
-        color = 256 - (base_color * x)
-        size = min_size * x * weight
+
+def draw_blob(frame, coordinates, time):
+    if time >= max_stationary_time:
+        layer = blob_layer
+    else:
+        layer = math.ceil(time * scale / layer_size)
+    for x in reversed(range(layer)):
+        color = color_start - (color_steps * x)
+        size = x * layer_size
         cv2.circle(frame, coordinates, int(size), (color, color, color), -1)
 
 heatmap = np.zeros((405, 720), dtype=np.uint8)
 for points in stationary_points:
     draw_heatmap = np.zeros((405, 720), dtype=np.uint8)
-    draw_blob(draw_heatmap, tuple(points[0]), points[1], 720)
+    draw_blob(draw_heatmap, tuple(points[0]), points[1])
     heatmap = cv2.add(heatmap, draw_heatmap)
 
+lo = np.array([color_start])
+hi = np.array([255])
+mask = cv2.inRange(heatmap, lo, hi)
+heatmap[mask > 0] = color_start
+
 heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-lo = np.array([128, 0, 0])
-hi = np.array([255, 0, 0])
+lo = np.array([128,0,0])
+hi = np.array([136,0,0])
 mask = cv2.inRange(heatmap, lo, hi)
 heatmap[mask > 0] = (0, 0, 0)
 
 for row in range(heatmap.shape[0]):
     for col in range(heatmap.shape[1]):
-        if not (heatmap[row][col] == np.array([0,0,0])).all():
-            heatmap_frame[row][col] = heatmap[row][col]
+        if (heatmap[row][col] == np.array([0,0,0])).all():
+            heatmap[row][col] = heatmap_frame[row][col] 
+
+heatmap_frame = cv2.addWeighted(heatmap, 0.75, heatmap_frame, 0.25, 1)
 
 cv2.imshow("Movement Tracks", tracks_frame)
 cv2.imshow("Stationary Location Heatmap", heatmap_frame)
