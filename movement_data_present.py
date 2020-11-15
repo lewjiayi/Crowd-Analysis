9279fdbf926993f8773177d1642eee09da01b72e
lewjiayi <jiaareaper@hotmail.com>
2020-11-16 00:04:53 +0800

    smoothen track trails

--- movement_data_present.py
+++ movement_data_present.py
@@ -11,7 +11,7 @@ from scipy.spatial.distance import euclidean
 from colors import RGB_COLORS, gradient_color_RGB
 
 tracks = []
-with open('processed_data/movement_data.csv', 'r') as file:
+with open('processed_data_5/movement_data.csv', 'r') as file:
     reader = csv.reader(file, delimiter=',')
     for row in reader:
         if len(row[3:]) > 4:
@@ -21,25 +21,18 @@ with open('processed_data/movement_data.csv', 'r') as file:
                 temp.append([int(data[i]), int(data[i+1])])
             tracks.append(temp)
 
-cap = cv2.VideoCapture(VIDEO_CONFIG["VIDEO_CAP"])
-cap.set(1, 100)
-(ret, tracks_frame) = cap.read()
-tracks_frame = imutils.resize(tracks_frame, width=720)
-heatmap_frame = np.copy(tracks_frame)
-
-color1 = (255, 96, 0)
-color2 = (0, 28, 255)
-for track in tracks:
-    for i in range(len(track) - 1):
-        color = gradient_color_RGB(color1, color2, len(track) - 1, i)
-        cv2.line(tracks_frame, tuple(track[i]), tuple(track[i+1]), color, 2)
-
-with open('processed_data/video_data.json', 'r') as file:
+with open('processed_data_5/video_data.json', 'r') as file:
 	data = json.load(file)
 	vid_fps = data["VID_FPS"]
 	data_record_frame = data["DATA_RECORD_FRAME"]
 	frame_size = data["PROCESSED_FRAME_SIZE"]
 
+cap = cv2.VideoCapture(VIDEO_CONFIG["VIDEO_CAP"])
+cap.set(1, 100)
+(ret, tracks_frame) = cap.read()
+tracks_frame = imutils.resize(tracks_frame, width=frame_size)
+heatmap_frame = np.copy(tracks_frame)
+print(tracks_frame.shape)
 stationary_threshold_seconds = 2
 stationary_threshold_frame =  round(vid_fps * stationary_threshold_seconds / data_record_frame)
 stationary_distance = frame_size * 0.05
@@ -52,20 +45,38 @@ color_end = 0
 color_steps = int((color_start - color_end) / blob_layer)
 scale = 1.5
 
-stationary_points = []
-for movement in tracks:
-	stationary = movement[0]
-	stationary_time = 0
-	for i in movement[1:]:
-		if euclidean(stationary, i) < stationary_distance:
-			stationary = i
-			stationary_time += 1
-		else:
-			if stationary_time > stationary_threshold_frame:
-				stationary_points.append([stationary, stationary_time])
-			stationary = i
-			stationary_time = 0
+# print(stationary_distance)
+# print(stationary_threshold_frame)
 
+stationary_points = []
+movement_points = []
+total = 0
+for movement in tracks:
+    temp_movement_point = [movement[0]]
+    stationary = movement[0]
+    stationary_time = 0
+    for i in movement[1:]:
+        if euclidean(stationary, i) < stationary_distance:
+            stationary_time += 1
+        else:
+            temp_movement_point.append(i)
+            if stationary_time > stationary_threshold_frame:
+                stationary_points.append([stationary, stationary_time])
+            stationary = i
+            stationary_time = 0
+    movement_points.append(temp_movement_point)
+    total += len(temp_movement_point)
+
+# print(total)
+# print(movement_points)
+
+color1 = (255, 96, 0)
+color2 = (0, 28, 255)
+for track in movement_points:
+    for i in range(len(track) - 1):
+        color = gradient_color_RGB(color1, color2, len(track) - 1, i)
+        cv2.line(tracks_frame, tuple(track[i]), tuple(track[i+1]), color, 2)
+    
 def draw_blob(frame, coordinates, time):
     if time >= max_stationary_time:
         layer = blob_layer
@@ -76,9 +87,9 @@ def draw_blob(frame, coordinates, time):
         size = x * layer_size
         cv2.circle(frame, coordinates, int(size), (color, color, color), -1)
 
-heatmap = np.zeros((405, 720), dtype=np.uint8)
+heatmap = np.zeros((heatmap_frame.shape[0], heatmap_frame.shape[1]), dtype=np.uint8)
 for points in stationary_points:
-    draw_heatmap = np.zeros((405, 720), dtype=np.uint8)
+    draw_heatmap = np.zeros((heatmap_frame.shape[0], heatmap_frame.shape[1]), dtype=np.uint8)
     draw_blob(draw_heatmap, tuple(points[0]), points[1])
     heatmap = cv2.add(heatmap, draw_heatmap)
 
